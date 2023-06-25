1010a2b373a6e9bac10139e560be436ce31e3baa
saikat0511 <saikat0511@gmail.com>
2023-06-25 20:38:56 +0530

    fix whitespace and use lowercase error message

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,8 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
+
 export const headers = require("./config/headers.json");
+
 export const templates = require("./config/templates.json");
+
 export const URLs = require("./config/URLs.json");
