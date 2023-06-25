7f2768fabcc62d223a0de3142d33626aa41fd4a6
saikat0511 <saikat0511@gmail.com>
2023-06-25 21:43:39 +0530

    fix username schema

--- src/resources/cookie/requestSchema.js
+++ src/resources/cookie/requestSchema.js
@@ -1,7 +1,7 @@
 import { object, string, number } from "yup";
 
 const cookieSchema = object({
-  username: number().integer().min(1000000).max(9999999).required(),
+  username: number().integer().min(1000000).required(),
   password: string().required(),
 });
 
