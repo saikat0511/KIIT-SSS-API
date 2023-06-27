eec4f66dffa9edceb18df6b75f8cc812df50595e
saikat0511 <saikat0511@gmail.com>
2023-06-27 16:12:51 +0530

    use PORT env var instead of hard coding it

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import v1Router from "./routes.js";
 
 const app = express();
+const PORT = process.env.PORT || 5555;
+
 
 app.use(express.json());
 app.use("/api/v1", v1Router);
 
-app.listen(5555, () => {
-  console.log("server is listening on port 5555");
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
 });
